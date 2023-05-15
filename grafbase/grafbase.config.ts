5729b060ffd0d1f55727bf302634c0b1e5bd616e
Julius de Bruijn <julius+github@nauk.io>
2023-05-15 16:54:58 +0200

    init

---
+++ grafbase/grafbase.config.ts
@@ -0,0 +1,22 @@
+import { g, config } from '@grafbase/sdk'
+
+enum Fruit {
+  Apple,
+  Orange
+}
+
+const fruit = g.enum("Fruit", Fruit);
+
+const profile = g.type("Profile", {
+  address: g.tring(),
+  favoriteFruit: g.ref(fruit).optional()
+})
+
+const user = g.model("User", {
+  name: g.string(),
+  age: g.int().optional(),
+  profile: g.ref(profile).optional(),
+  parent: g.relation(() => user).optional(),
+})
+
+export default config({ schema: g })
